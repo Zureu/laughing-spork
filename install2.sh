946258338c600186f70e6daa06810a944ea104d8
Zureu <muharomifajar130@gmail.com>
2025-05-18 13:58:18 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,5 +1,5 @@
 pkg update && pkg upgrade -y
-wget -P $PREFIX/bin https://raw.githubusercontent.com/Zureu/laughing-spork/refs/heads/main/ChDDoS.py
+wget -P $PREFIX/bin https://raw.githubusercontent.com/Zureu/laughing-spork/refs/heads/main/XDDoS.py
 mv $PREFIX/bin/XDDoS.py  $PREFIX/bin/XDDoS
 chmod +x $PREFIX/bin/XDDoS
 XDDoS
